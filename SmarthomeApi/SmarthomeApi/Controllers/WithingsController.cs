d65ad3da52526c8ed5ef344d4b61fb59498555dd
philipdaubmeier <philip@daubmeier.de>
2019-01-08 20:28:25 +0000

    Created a callback for registering a Withings Developer Account
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C238

---
+++ SmarthomeApi/SmarthomeApi/Controllers/WithingsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmarthomeApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/withings")]
+    public class WithingsController : Controller
+    {
+        private const string clientId = "***REMOVED***";
+        private const string consumerSecret = "***REMOVED***";
+
+        // POST: api/withings/callback
+        [HttpPost("callback")]
+        public IActionResult PostCallback([FromForm]string formdata, [FromBody]string body)
+        {
+            return StatusCode(200);
+        }
+
+        // HEAD: api/withings/callback
+        [HttpHead("callback")]
+        public IActionResult HeadCallback()
+        {
+            return StatusCode(200);
+        }
+    }
+}
