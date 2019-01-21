fb4dc515685fd8412cf0fef05d436212c2199415
philipdaubmeier <philip@daubmeier.de>
2019-01-21 21:41:35 +0000

    Added database controller for reading data from the db, secured with a hardcoded password
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C252

---
+++ SmarthomeApi/SmarthomeApi/Controllers/DatabaseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmarthomeApi.Database.Model;
+
+namespace SmarthomeApi.Controllers
+{
+    [Route("api/database")]
+    public class DatabaseController : Controller
+    {
+        private readonly PersistenceContext db;
+        public DatabaseController(PersistenceContext databaseContext)
+        {
+            db = databaseContext;
+        }
+
+        // GET api/database/authdatasets
+        [HttpGet("authdatasets")]
+        public async Task<ActionResult> Get(string password)
+        {
+            if (password != "9bb04cf87b69e851")
+                return StatusCode(403);
+
+            var authdata = db.AuthDataSet.ToList();
+
+            return Json(new
+            {
+                authdata = authdata.Select(x => new
+                {
+                    id = x.AuthDataId,
+                    value = !x.AuthDataId.EndsWith("_expiry") ? x.DataContent: DateTime.FromBinary(long.Parse(x.DataContent)).ToString("o")
+                })
+            });
+        }
+    }
+}
