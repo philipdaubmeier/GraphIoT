2b9f0a437d384aa3e7418e9fdd5ba743c32ee643
philipdaubmeier <philip@daubmeier.de>
2019-02-26 19:52:24 +0000

    Implemented Viessmann Estrella client and added an API for accessing the gateway and controller names.
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C272

---
+++ SmarthomeApi/SmarthomeApi/Clients/Viessmann/ViessmannEstrellaClient.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmarthomeApi.Clients.Viessmann
+{
+    public class ViessmannEstrellaClient
+    {
+        private HttpClient _client = new HttpClient();
+
+        public async Task<List<int>> GetGateways()
+        {
+            return await ParseGatewaysResponse(await GetAsync(
+                new Uri("https://api.viessmann.io/estrella/rest/v2.0/gateways/")));
+        }
+
+        public async Task<List<Tuple<int, string>>> GetControllers(int gatewayId)
+        {
+            return await ParseControllersResponse(await GetAsync(
+                new Uri($"https://api.viessmann.io/estrella/rest/v2.0/gateways/{gatewayId}/controllers")));
+        }
+        
+        private async Task<HttpResponseMessage> GetAsync(Uri uri)
+        {
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = uri,
+                Method = HttpMethod.Get,
+            };
+
+            var username = "***REMOVED***";
+            var password = "***REMOVED***";
+            var basicAuth = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", basicAuth);
+            
+            return await _client.SendAsync(request);
+        }
+
+        private async Task<List<int>> ParseGatewaysResponse(HttpResponseMessage response)
+        {
+            var definition = new
+            {
+                data = new[] { new
+                {
+                    id = "",
+                    type = "",
+                    ownerId = "",
+                    name = "",
+                    gatewaySerial = "",
+                    macAddress = "",
+                    street = "",
+                    houseNumber = "",
+                    zip = "",
+                    city = "",
+                    country = "",
+                    region = "",
+                    trackingNumber = "",
+                    isRegistered = true,
+                    isActive = true,
+                    status = "",
+                    autoUpdate = false,
+                    firmwareVersion = "",
+                    hasErrors = false,
+                    producedAt = "",
+                    lastCommunicationAt = "",
+                    connectedAt = "",
+                    disconnectedAt = "",
+                    registeredAt = "",
+                    createdAt = "",
+                    editedAt = "",
+                    latitude = 0d,
+                    longitude = 0d,
+                    timeZone = "",
+                    equipmentNumber = "",
+                    isDeleted = false,
+                    wifiStrength = 0
+                } },
+                paging = new
+                {
+                    offset = 0,
+                    limit = 0,
+                    total = 0
+                }
+            };
+            var gatewaysRaw = JsonConvert.DeserializeAnonymousType(await response.Content.ReadAsStringAsync(), definition);
+            return gatewaysRaw.data.Select(x => int.Parse(x.id)).ToList();
+        }
+
+        private async Task<List<Tuple<int, string>>> ParseControllersResponse(HttpResponseMessage response)
+        {
+            var definition = new
+            {
+                data = new[] { new
+                {
+                    id = "",
+                    boilerSerial = "",
+                    bmuSerial = "",
+                    typeId = "",
+                    typeName = "",
+                    hasError = false,
+                    isActive = true,
+                    gatewayId = "",
+                    categoryFamilyIndex = 0,
+                    status = "",
+                    synchronizationStatus = ""
+                } }
+            };
+            var controllersRaw = JsonConvert.DeserializeAnonymousType(await response.Content.ReadAsStringAsync(), definition);
+            return controllersRaw.data.Select(x => new Tuple<int, string>(int.Parse(x.id), x.typeName)).ToList();
+        }
+    }
+}
---
+++ SmarthomeApi/SmarthomeApi/Controllers/ViessmannController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmarthomeApi.Clients.Viessmann;
+
+namespace SmarthomeApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/viessmann")]
+    public class ViessmannController : Controller
+    {
+        private readonly ViessmannEstrellaClient estrellaClient = new ViessmannEstrellaClient();
+
+        // GET: api/viessmann/installations/names
+        [HttpGet("installations/names")]
+        public async Task<JsonResult> GetInstallationNames()
+        {
+            var gatewayIds = await estrellaClient.GetGateways();
+            var controllers = new List<Tuple<int, List<Tuple<int, string>>>>();
+            foreach (var gatewayId in gatewayIds)
+                controllers.Add(new Tuple<int, List<Tuple<int, string>>>(gatewayId,
+                    await estrellaClient.GetControllers(gatewayId)));
+
+            return Json(new
+            {
+                gateways = controllers.Select(g => new
+                {
+                    id = g.Item1,
+                    controllers = g.Item2.Select(c => new
+                    {
+                        id = c.Item1,
+                        name = c.Item2
+                    })
+                })
+            });
+        }
+    }
+}
