be5b16e65fcc1c269b989c33f1bb67238911ae0f
philipdaubmeier <philip@daubmeier.de>
2019-03-01 21:52:29 +0000

    Implemented interface for accessing a solar curves/timeseries of a specific day. Implemented interface for lametric view of solarthermic data, like total yield today, the yield of the last 3 hours as well as the current collector temperature.
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C288

--- SmarthomeApi/SmarthomeApi/Controllers/ViessmannController.cs
+++ SmarthomeApi/SmarthomeApi/Controllers/ViessmannController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -78,19 +79,79 @@ namespace SmarthomeApi.Controllers
             });
         }
 
-        // GET: api/viessmann/solar
-        [HttpGet("solar")]
-        public async Task<JsonResult> GetSolar()
+        // GET: api/viessmann/solar/lametric
+        [HttpGet("solar/lametric")]
+        public ActionResult GetSolarLaMetric()
         {
-            var data = await vitotrolClient.GetData(new List<int>() { 7895 });
+            var dbSolarSeries = db.ViessmannSolarTimeseries.Where(x => x.Day == DateTime.Now.Date).FirstOrDefault();
+            if (dbSolarSeries == null)
+                return StatusCode(404);
+
+            var totalYieldToday = (double)dbSolarSeries.SolarWhTotal / 1000d;
+            var chartSolarYield = dbSolarSeries.SolarWhSeries.SkipWhile(x => !x.Value.HasValue)
+                .Reverse().SkipWhile(x => !x.Value.HasValue).Reverse()
+                .Select(x => x.Value ?? 0).TakeLast(37).ToList();
+            var currentCollectorTemp = dbSolarSeries.SolarCollectorTempSeries.Reverse()
+                .SkipWhile(x => !x.Value.HasValue).FirstOrDefault().Value;
 
             return Json(new
             {
-                datapoints = data.Select(x => new
+                frames = new List<object>() {
+                    new
+                    {
+                        text = $"{totalYieldToday:0.#} kWh",
+                        icon = "a27283"
+                    },
+                    new
+                    {
+                        index = 2,
+                        chartData = chartSolarYield
+                    },
+                    new
+                    {
+                        text = $"{currentCollectorTemp:0.#} °C",
+                        icon = "a27285"
+                    }
+                }
+            });
+        }
+
+        // GET: api/viessmann/solar/curves/days/{day}
+        [HttpGet("solar/curves/days/{day}")]
+        public ActionResult GetSolarCurves([FromRoute] string day)
+        {
+            if (!DateTime.TryParseExact(day, "yyyy'-'MM'-'dd", CultureInfo.InvariantCulture,
+                DateTimeStyles.AssumeLocal, out DateTime dayDate))
+                return StatusCode(404);
+            
+            var dbSolarSeries = db.ViessmannSolarTimeseries.Where(x => x.Day == dayDate.Date).FirstOrDefault();
+            if (dbSolarSeries == null)
+                return StatusCode(404);
+
+            var l1 = dbSolarSeries.SolarWhSeries.ToList();
+            var l2 = dbSolarSeries.SolarCollectorTempSeries.ToList();
+            var l3 = dbSolarSeries.SolarHotwaterTempSeries.ToList();
+            var l4 = dbSolarSeries.SolarPumpStateSeries.ToList();
+            var l5 = dbSolarSeries.SolarSuppressionSeries.ToList();
+            var zippedValues = new List<Tuple<DateTime, int?, double?, double?, bool?, bool?>>();
+            for (int i = 0; i < l1.Count; i++)
+                zippedValues.Add(new Tuple<DateTime, int?, double?, double?, bool?, bool?>(
+                    l1[i].Key, l1[i].Value, l2[i].Value, l3[i].Value, l4[i].Value, l5[i].Value));
+
+            Func<Tuple<DateTime, int?, double?, double?, bool?, bool?>, bool> hasValue = 
+                t => t.Item2.HasValue || t.Item3.HasValue || t.Item4.HasValue || t.Item5.HasValue || t.Item6.HasValue;
+            var zippedTrimmed = zippedValues.SkipWhile(t => !hasValue(t)).Reverse().SkipWhile(t => !hasValue(t)).Reverse().ToList();
+
+            return Json(new
+            {
+                begin = zippedTrimmed.FirstOrDefault()?.Item1 ?? l1.FirstOrDefault().Key,
+                solardata = zippedTrimmed.Select(x => new
                 {
-                    id = x.Item1,
-                    value = x.Item2,
-                    timestamp = x.Item3
+                    wh = x.Item2,
+                    collector_temp = x.Item3,
+                    dhw_temp = x.Item4,
+                    pump_state = x.Item5,
+                    suppression = x.Item6
                 })
             });
         }
