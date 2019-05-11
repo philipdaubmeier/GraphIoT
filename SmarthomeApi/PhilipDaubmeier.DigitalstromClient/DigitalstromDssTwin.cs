48741d286cb2b0e36a2583fa56a22c42202d6830
philipdaubmeier <philip@daubmeier.de>
2019-05-11 21:33:04 +0000

    Added the SceneChanged event to Twin directly.
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C564

--- SmarthomeApi/PhilipDaubmeier.DigitalstromClient/DigitalstromDssTwin.cs
+++ SmarthomeApi/PhilipDaubmeier.DigitalstromClient/DigitalstromDssTwin.cs
@@ -6,20 +6,26 @@ namespace PhilipDaubmeier.DigitalstromClient
     public class DigitalstromDssTwin : ApartmentState, IDisposable
     {
         private readonly DssEventSubscriber _subscriber;
-        private readonly TwinChangeAggregator _sceneCaller;
+        private readonly TwinChangeAggregator _changeAggregator;
+
+        public event SceneChangedEventHandler SceneChanged
+        {
+            add { _changeAggregator.SceneChanged += value; }
+            remove { _changeAggregator.SceneChanged -= value; }
+        }
 
         public DigitalstromDssTwin(IDigitalstromConnectionProvider connectionProvider)
         {
             _subscriber = connectionProvider is null ? null : new DssEventSubscriber(connectionProvider, this);
-            _sceneCaller = _subscriber is null ? null : new TwinChangeAggregator(this);
+            _changeAggregator = _subscriber is null ? null : new TwinChangeAggregator(this);
 
-            _sceneCaller.SceneChangedInternal += (s, e) => _subscriber.CallScene(e.Zone, e.Group, e.Scene);
+            _changeAggregator.SceneChangedInternal += (s, e) => _subscriber.CallScene(e.Zone, e.Group, e.Scene);
         }
 
         public void Dispose()
         {
             _subscriber?.Dispose();
-            _sceneCaller?.Dispose();
+            _changeAggregator?.Dispose();
         }
     }
 }
