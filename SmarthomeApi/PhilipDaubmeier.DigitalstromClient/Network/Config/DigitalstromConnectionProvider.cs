dd82bd9baed2cc81ca3cc6934322fce79ea8ecb5
philipdaubmeier <philip@daubmeier.de>
2019-05-11 22:21:45 +0000

    Added convenience constructor
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C565

--- SmarthomeApi/PhilipDaubmeier.DigitalstromClient/Network/Config/DigitalstromConnectionProvider.cs
+++ SmarthomeApi/PhilipDaubmeier.DigitalstromClient/Network/Config/DigitalstromConnectionProvider.cs
@@ -1,5 +1,7 @@
 ﻿using PhilipDaubmeier.DigitalstromClient.Model.Auth;
 using PhilipDaubmeier.DigitalstromClient.Network;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 
@@ -11,6 +13,10 @@ namespace PhilipDaubmeier.DigitalstromClient
         public IDigitalstromAuth AuthData { get; private set; }
         public X509Certificate2 ServerCertificate { get; private set; }
         public HttpMessageHandler Handler { get; private set; }
+
+        public DigitalstromConnectionProvider(Uri uri, IDigitalstromAuth authData, X509Certificate2 cert = null, HttpMessageHandler handler = null)
+            : this(new UriPriorityList(new List<Uri>() { uri }), authData, cert, handler)
+        { }
 
         public DigitalstromConnectionProvider(UriPriorityList uris, IDigitalstromAuth authData, X509Certificate2 cert = null, HttpMessageHandler handler = null)
         {
