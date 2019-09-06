6f0e7f5c88c5cfa289b74ea339c64cc839ff372d
philipdaubmeier <philip@daubmeier.de>
2019-09-06 21:08:59 +0000

    Added parameters for the other two netatmo apis and added documentation.
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C747

--- SmarthomeApi/PhilipDaubmeier.NetatmoClient/NetatmoWebClient.cs
+++ SmarthomeApi/PhilipDaubmeier.NetatmoClient/NetatmoWebClient.cs
@@ -20,16 +20,35 @@ namespace PhilipDaubmeier.NetatmoClient
             : base(connectionProvider)
         { }
 
-        public async Task<WeatherStationData> GetWeatherStationData()
+        /// <summary>
+        /// Returns data from a user's Weather Stations (measures and device specific data).
+        /// </summary>
+        /// <param name="deviceId">Weather station mac address. If null, all devices (or all favorites) are returned.</param>
+        /// <param name="getFavorites">To retrieve user's favorite weather stations. Default is false.</param>
+        public async Task<WeatherStationData> GetWeatherStationData(ModuleId deviceId = default, bool? getFavorites = default)
         {
             var uri = new Uri($"{_baseUri}/api/getstationsdata");
-            return await CallNetatmoApi<WeatherStationResponse, WeatherStationData>(uri);
+            return await CallNetatmoApi<WeatherStationResponse, WeatherStationData>(uri, new Dictionary<string, string>()
+            {
+                { "device_id", deviceId?.ToString() },
+                { "get_favorites", getFavorites?.ToString() }
+            });
         }
 
-        public async Task<HomeData> GetHomeData()
+        /// <summary>
+        /// Returns information about users homes and cameras. This method is available
+        /// for Welcome, Presence and the Smart Smoke Alarm.
+        /// </summary>
+        /// <param name="homeId">Specify if you're looking for the events of a specific Home. Example: "56c881f049c75fe562732029"</param>
+        /// <param name="size">Number of events to retrieve. Default is 30.</param>
+        public async Task<HomeData> GetHomeData(string homeId = default, int? size = default)
         {
             var uri = new Uri($"{_baseUri}/api/gethomedata");
-            return await CallNetatmoApi<HomeDataResponse, HomeData>(uri);
+            return await CallNetatmoApi<HomeDataResponse, HomeData>(uri, new Dictionary<string, string>()
+            {
+                { "home_id", homeId },
+                { "size", !size.HasValue ? null : Math.Max(0, Math.Min(1024, size.Value)).ToString() }
+            });
         }
 
         /// <summary>
@@ -43,7 +62,6 @@ namespace PhilipDaubmeier.NetatmoClient
         /// <param name="dateEnd">Timestamp of the last measure to retrieve (default and max are 1024). Default is null.</param>
         /// <param name="limit">Maximum number of measurements (default and max are 1024)</param>
         /// <param name="realTime">If scale different than max, timestamps are by default offset + scale/2. To get exact timestamps, use true. Default is false.</param>
-        /// <returns></returns>
         public async Task<List<TimestampedMeasureCollection>> GetMeasure(ModuleId deviceId, ModuleId moduleId, IEnumerable<Measure> types, Scale scale = default, DateTime? dateBegin = default, DateTime? dateEnd = default, int? limit = default, bool? realTime = default)
         {
             var uri = new Uri($"{_baseUri}/api/getmeasure");
