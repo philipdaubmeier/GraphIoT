e737c14d1e0135707a6d716140c8c9154e64f892
philipdaubmeier <philip@daubmeier.de>
2019-09-06 20:03:40 +0000

    Cleaned up model
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C743

--- SmarthomeApi/PhilipDaubmeier.NetatmoClient/Model/Measure/TimestampedMeasureCollection.cs
+++ SmarthomeApi/PhilipDaubmeier.NetatmoClient/Model/Measure/TimestampedMeasureCollection.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace PhilipDaubmeier.NetatmoClient.Model.HomeData
 {
     public class TimestampedMeasureCollection
     {
-        public int BegTime { get; set; }
-        public int StepTime { get; set; }
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime BegTime { get; set; }
+
+        [JsonConverter(typeof(SecondsTimeSpanConverter))]
+        public TimeSpan StepTime { get; set; }
+
         public List<List<double?>> Value { get; set; }
     }
 }
