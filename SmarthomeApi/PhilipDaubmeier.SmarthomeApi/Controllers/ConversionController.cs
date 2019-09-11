44ba531c38157b0d02177d449afa87d533f28078
philipdaubmeier <philip@daubmeier.de>
2019-09-11 20:33:10 +0000

    Implemented conversion for high energy time series.
    git-tfs-id: [https://philipdaubmeier.visualstudio.com]$/SmarthomeAPI;C776

--- SmarthomeApi/PhilipDaubmeier.SmarthomeApi/Controllers/ConversionController.cs
+++ SmarthomeApi/PhilipDaubmeier.SmarthomeApi/Controllers/ConversionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PhilipDaubmeier.CompactTimeSeries;
+using PhilipDaubmeier.DigitalstromHost.Database;
 using PhilipDaubmeier.SmarthomeApi.Database;
 using PhilipDaubmeier.TimeseriesHostCommon.Database;
 using PhilipDaubmeier.TimeseriesHostCommon.Parsers;
@@ -49,7 +50,25 @@ namespace PhilipDaubmeier.SmarthomeApi.Controllers
 
             DateTime FromLocalToUtc(DateTime dt) => new DateTime(dt.Ticks, DateTimeKind.Local).ToUniversalTime();
 
-            void ConvertOneTimeSeriesOneAndPrevDay<Tdb, Tval>(DateTime day, DbSet<Tdb> dbSet, Func<Tdb, Tdb, bool> dbComparer, int index, Func<Tdb, TimeSeries<Tval>> seriesSelector) where Tdb : TimeSeriesDbEntityBase where Tval : struct
+            void ConvertOneTimeSeriesOneAndPrevDay<Tval>(ITimeSeries<Tval> orig, ITimeSeries<Tval> origPrev) where Tval : struct
+            {
+                DateTime? lastTime = null;
+                foreach (var item in orig.Select(x => x.Key).ToList())
+                {
+                    lastTime = FromLocalToUtc(item);
+                    orig[lastTime.Value] = orig[item];
+                    if (origPrev != null)
+                        origPrev[lastTime.Value] = orig[item];
+                }
+
+                if (lastTime.HasValue)
+                {
+                    foreach (var time in Enumerable.Range(1, (int)(60 * 60 * 2 / orig.Span.Duration.TotalSeconds)).Select(i => lastTime.Value + i * orig.Span.Duration))
+                        orig[time] = null;
+                }
+            }
+
+            void ConvertAllTimeSeriesOneAndPrevDay<Tdb, Tval>(DateTime day, DbSet<Tdb> dbSet, Func<Tdb, Tdb, bool> dbComparer, int index, Func<Tdb, TimeSeries<Tval>> seriesSelector) where Tdb : TimeSeriesDbEntityBase where Tval : struct
             {
                 var dateCurr = day;
                 var datePrev = dateCurr.AddDays(-1);
@@ -58,51 +77,62 @@ namespace PhilipDaubmeier.SmarthomeApi.Controllers
                 {
                     var orig = seriesSelector(pair.Item1);
                     var origPrev = pair.Item2 == null ? null : seriesSelector(pair.Item2);
-                    DateTime? lastTime = null;
-                    foreach (var item in orig.Select(x => x.Key).ToList())
-                    {
-                        lastTime = FromLocalToUtc(item);
-                        orig[lastTime.Value] = orig[item];
-                        if (origPrev != null)
-                            origPrev[lastTime.Value] = orig[item];
-                    }
 
-                    if (lastTime.HasValue)
-                    {
-                        foreach (var time in Enumerable.Range(1, (int)(60 * 60 * 2 / orig.Span.Duration.TotalSeconds)).Select(i => lastTime.Value + i * orig.Span.Duration))
-                            orig[time] = null;
-                    }
+                    ConvertOneTimeSeriesOneAndPrevDay(orig, origPrev);
 
                     pair.Item1.SetSeries(index, orig);
                     pair.Item2?.SetSeries(index, origPrev);
                 }
             }
 
+            void ConvertTimeSeriesCollectionOneAndPrevDay(DateTime day, DbSet<DigitalstromEnergyHighresData> dbSet)
+            {
+                var dateCurr = day;
+                var datePrev = dateCurr.AddDays(-1);
+                var loaded = dbSet.Where(x => x.Key == dateCurr || x.Key == datePrev).ToList();
+
+                var dbCurr = loaded.Where(x => x.Key == dateCurr).FirstOrDefault();
+                var dbPrev = loaded.Where(x => x.Key == datePrev).FirstOrDefault();
+                var loadedCurr = dbCurr?.EnergySeriesEveryMeter;
+                var loadedPrev = dbPrev?.EnergySeriesEveryMeter;
+                if (loadedCurr == null)
+                    return;
+
+                foreach (var pair in loadedCurr.Select(x => new Tuple<ITimeSeries<int>, ITimeSeries<int>>(x.Value, loadedPrev?.Where(y => x.Key == y.Key)?.FirstOrDefault().Value)))
+                    ConvertOneTimeSeriesOneAndPrevDay(pair.Item1, pair.Item2);
+
+                dbCurr.EnergySeriesEveryMeter = loadedCurr;
+                if (dbPrev != null)
+                    dbPrev.EnergySeriesEveryMeter = loadedPrev;
+            }
+
             foreach (var day in span.IncludedDates().Select(x => x.ToUniversalTime()))
             {
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.DsSensorDataSet, (db1, db2) => db1.ZoneId == db2.ZoneId, 0, db => db.TemperatureSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.DsSensorDataSet, (db1, db2) => db1.ZoneId == db2.ZoneId, 1, db => db.HumiditySeries);
+                ConvertTimeSeriesCollectionOneAndPrevDay(day, db.DsEnergyHighresDataSet);
 
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 0, db => db.SolarWhSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 1, db => db.SolarCollectorTempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 2, db => db.SolarHotwaterTempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 3, db => db.SolarPumpStateSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 4, db => db.SolarSuppressionSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.DsSensorDataSet, (db1, db2) => db1.ZoneId == db2.ZoneId, 0, db => db.TemperatureSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.DsSensorDataSet, (db1, db2) => db1.ZoneId == db2.ZoneId, 1, db => db.HumiditySeries);
 
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 0, db => db.BurnerMinutesSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 1, db => db.BurnerStartsSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 2, db => db.BurnerModulationSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 3, db => db.OutsideTempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 4, db => db.BoilerTempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 5, db => db.BoilerTempMainSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 6, db => db.Circuit0TempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 7, db => db.Circuit1TempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 8, db => db.DhwTempSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 9, db => db.BurnerActiveSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 10, db => db.Circuit0PumpSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 11, db => db.Circuit1PumpSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 12, db => db.DhwPrimaryPumpSeries);
-                ConvertOneTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 13, db => db.DhwCirculationPumpSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 0, db => db.SolarWhSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 1, db => db.SolarCollectorTempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 2, db => db.SolarHotwaterTempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 3, db => db.SolarPumpStateSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannSolarTimeseries, (db1, db2) => true, 4, db => db.SolarSuppressionSeries);
+
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 0, db => db.BurnerMinutesSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 1, db => db.BurnerStartsSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 2, db => db.BurnerModulationSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 3, db => db.OutsideTempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 4, db => db.BoilerTempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 5, db => db.BoilerTempMainSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 6, db => db.Circuit0TempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 7, db => db.Circuit1TempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 8, db => db.DhwTempSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 9, db => db.BurnerActiveSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 10, db => db.Circuit0PumpSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 11, db => db.Circuit1PumpSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 12, db => db.DhwPrimaryPumpSeries);
+                ConvertAllTimeSeriesOneAndPrevDay(day, db.ViessmannHeatingTimeseries, (db1, db2) => true, 13, db => db.DhwCirculationPumpSeries);
 
                 db.SaveChanges();
             }
