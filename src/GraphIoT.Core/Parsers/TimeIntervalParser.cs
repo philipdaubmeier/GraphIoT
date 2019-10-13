461f7dcb6c5d0da065093506cc6eefd886ce257c
philipdaubmeier <philip@daubmeier.de>
2019-10-13 21:27:03 +0200

    Fixed copy-paste bug in TimeIntevalParser

--- src/GraphIoT.Core/Parsers/TimeIntervalParser.cs
+++ src/GraphIoT.Core/Parsers/TimeIntervalParser.cs
@@ -13,8 +13,8 @@ namespace PhilipDaubmeier.GraphIoT.Core.Parsers
             var match = Regex.Match(interval, @"^((?<w>\d+)w)?((?<d>\d+)d)?((?<h>\d+)h)?((?<m>\d+)m)?((?<s>\d+)s)?((?<ms>\d+)ms)?$",
                 RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.RightToLeft);
 
-            var w = match.Groups["w"].Success ? int.Parse(match.Groups["h"].Value) : 0;
-            var d = match.Groups["d"].Success ? int.Parse(match.Groups["h"].Value) : 0;
+            var w = match.Groups["w"].Success ? int.Parse(match.Groups["w"].Value) : 0;
+            var d = match.Groups["d"].Success ? int.Parse(match.Groups["d"].Value) : 0;
             var h = match.Groups["h"].Success ? int.Parse(match.Groups["h"].Value) : 0;
             var m = match.Groups["m"].Success ? int.Parse(match.Groups["m"].Value) : 0;
             var s = match.Groups["s"].Success ? int.Parse(match.Groups["s"].Value) : 0;
