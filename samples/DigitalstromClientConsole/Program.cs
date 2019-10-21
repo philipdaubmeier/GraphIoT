01e66fc0c54f8c6d6946e4d96dd967106385cbee
philipdaubmeier <philip@daubmeier.de>
2019-10-21 23:43:05 +0200

    Enabled nullable reference types for console sample project.

--- samples/DigitalstromClientConsole/Program.cs
+++ samples/DigitalstromClientConsole/Program.cs
@@ -29,31 +29,29 @@ namespace PhilipDaubmeier.DigitalstromClientConsole
 
             Uri uri = ConsoleUtil.ReadUri("Enter DSS uri: (e.g. 'https://dss.local:8080')");
 
-            IDigitalstromAuth loginUser()
+            static IDigitalstromAuth loginUser()
             {
                 var username = ConsoleUtil.ReadNonWhitespace("Enter DSS username:");
                 var password = ConsoleUtil.ReadPassword("Enter DSS password:");
                 return new EphemeralDigitalstromAuth("ConsoleSample", username, password);
             }
 
-            bool validateCert(X509Certificate2 cert)
+            static bool validateCert(X509Certificate2 cert)
             {
                 Console.WriteLine($"Certificate fingerprint: {cert.GetCertHashString()}");
                 Console.WriteLine("Trust this certificate? (y/n)");
                 return Console.ReadLine().ToLowerInvariant().Trim().StartsWith('y');
             }
 
-            var conn = new DigitalstromConnectionProvider(uri, loginUser, validateCert);
+            using var conn = new DigitalstromConnectionProvider(uri, loginUser, validateCert);
 
-            using (var client = new DigitalstromDssClient(conn))
-            {
-                var zone = ConsoleUtil.ReadZone("Enter a room id (zone):");
-                var group = ConsoleUtil.ReadGroup("Enter a color id:");
-                var reachableScenes = await client.GetReachableScenes(zone, group);
+            var client = new DigitalstromDssClient(conn);
+            var zone = ConsoleUtil.ReadZone("Enter a room id (zone):");
+            var group = ConsoleUtil.ReadGroup("Enter a color id:");
+            var reachableScenes = await client.GetReachableScenes(zone, group);
 
-                Console.WriteLine($"Reachable scenes in zone '{zone}', group '{group}':");
-                Console.WriteLine(string.Join("\n", reachableScenes.ReachableScenes.Select(s => "\t" + s.ToDisplayString())));
-            }
+            Console.WriteLine($"Reachable scenes in zone '{zone}', group '{group}':");
+            Console.WriteLine(string.Join("\n", reachableScenes.ReachableScenes.Select(s => "\t" + s.ToDisplayString())));
         }
     }
 }
