ed6d2b095c463c1333d4d536d71b92e1b2ada0b1
philipdaubmeier <philip@daubmeier.de>
2020-03-20 23:56:42 +0100

    Added convenience function to quickly find values to a given feature name and optionally circuit number.

--- src/ViessmannClient/Model/Features/FeatureList.cs
+++ src/ViessmannClient/Model/Features/FeatureList.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PhilipDaubmeier.ViessmannClient.Model.Features
 {
     public class FeatureList
     {
         public List<Feature>? Features { get; set; }
+
+        public PropertyList? GetFeature(FeatureName.Name name, FeatureName.Circuit? circuit = FeatureName.Circuit.Circuit0)
+        {
+            var nameStr = (string)new FeatureName(name, circuit);
+            return Features.Where(x => x.Name?.Equals(nameStr, System.StringComparison.InvariantCultureIgnoreCase) ?? false).FirstOrDefault()?.Properties;
+        }
     }
 }
