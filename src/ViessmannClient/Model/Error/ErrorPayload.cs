d613b1cf50ca01dabe78920389bd087802ed31e0
philipdaubmeier <philip@daubmeier.de>
2020-03-20 23:24:32 +0100

    Added model classes for error payloads.

---
+++ src/ViessmannClient/Model/Error/ErrorPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace PhilipDaubmeier.ViessmannClient.Model.Error
+{
+    public class ErrorPayload
+    {
+        public string? ViErrorId { get; set; }
+        public int? StatusCode { get; set; }
+        public string? ErrorType { get; set; }
+        public string? Message { get; set; }
+        public ExtendedErrorPayload? ExtendedPayload { get; set; }
+    }
+}
