a208ef0dc30892571f7ecee0c52cb12c30c6d970
philipdaubmeier <philip@daubmeier.de>
2020-03-21 20:30:17 +0100

    Fixed bug due to missing json name mappings.

--- src/ViessmannClient/Model/Auth/AccessTokenResponse.cs
+++ src/ViessmannClient/Model/Auth/AccessTokenResponse.cs
@@ -1,9 +1,16 @@
-﻿namespace PhilipDaubmeier.ViessmannClient.Model.Auth
+﻿using Newtonsoft.Json;
+
+namespace PhilipDaubmeier.ViessmannClient.Model.Auth
 {
     public class AccessTokenResponse
     {
+        [JsonProperty("access_token")]
         public string? AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
         public string? ExpiresIn { get; set; }
+
+        [JsonProperty("token_type")]
         public string? TokenType { get; set; }
     }
 }
