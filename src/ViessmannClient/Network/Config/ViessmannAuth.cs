a0152c1cd481a064bd6d5930107ee23d08397598
philipdaubmeier <philip@daubmeier.de>
2020-03-22 22:44:22 +0100

    Added default implementation of IViessmannAuth as a preparation for unit tests.

---
+++ src/ViessmannClient/Network/Config/ViessmannAuth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PhilipDaubmeier.ViessmannClient.Network
+{
+    public class ViessmannAuth : IViessmannAuth
+    {
+        public string? AccessToken { get; private set; }
+        public DateTime AccessTokenExpiry { get; private set; }
+
+        public string Username { get; private set; }
+        public string UserPassword { get; private set; }
+
+        public ViessmannAuth(string user, string password)
+        {
+            Username = user;
+            UserPassword = password;
+
+            AccessToken = null;
+            AccessTokenExpiry = DateTime.MinValue;
+        }
+
+        public bool IsAccessTokenValid() => !string.IsNullOrEmpty(AccessToken) && AccessTokenExpiry > DateTime.Now;
+
+        public Task UpdateTokenAsync(string? accessToken, DateTime accessTokenExpiry, string? refreshToken)
+        {
+            AccessToken = accessToken;
+            AccessTokenExpiry = accessTokenExpiry;
+            return Task.CompletedTask;
+        }
+    }
+}
