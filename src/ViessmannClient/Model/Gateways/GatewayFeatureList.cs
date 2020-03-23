9bacaf453666565e55dca23884cf9e9503026e19
philipdaubmeier <philip@daubmeier.de>
2020-03-23 21:22:24 +0100

    Added GatewayFeatureList class for strongly typed access to gateway features.

---
+++ src/ViessmannClient/Model/Gateways/GatewayFeatureList.cs
@@ -0,0 +1,44 @@
+﻿using PhilipDaubmeier.ViessmannClient.Model.Devices;
+using System;
+using System.Collections.Generic;
+
+namespace PhilipDaubmeier.ViessmannClient.Model.Features
+{
+    public class GatewayFeatureList : FeatureList
+    {
+        public bool IsGatewayStatusOnline()
+        {
+            return GetProperties(FeatureName.Name.GatewayStatus)?.Online?.Value ?? false;
+        }
+
+        public decimal GetGatewayWifiStrength()
+        {
+            return GetProperties(FeatureName.Name.GatewayWifi)?.Strength?.Value ?? 0m;
+        }
+
+        public int GetGatewayWifiChannel()
+        {
+            return GetProperties(FeatureName.Name.GatewayWifi)?.Channel?.Value ?? 0;
+        }
+
+        public bool IsGatewayBmuconnectionOk()
+        {
+            return GetProperties(FeatureName.Name.GatewayBmuconnection)?.Status?.Value?.Equals("ok", StringComparison.InvariantCultureIgnoreCase) ?? false;
+        }
+
+        public IEnumerable<Device> GetGatewayDevices()
+        {
+            return GetProperties(FeatureName.Name.GatewayDevices)?.Devices?.Value ?? new List<Device>();
+        }
+
+        public string GetGatewayFirmwareVersion()
+        {
+            return GetProperties(FeatureName.Name.GatewayFirmware)?.Version?.Value as string ?? string.Empty;
+        }
+
+        public string GetGatewayFirmwareUpdateStatus()
+        {
+            return GetProperties(FeatureName.Name.GatewayFirmware)?.UpdateStatus?.Value as string ?? string.Empty;
+        }
+    }
+}
