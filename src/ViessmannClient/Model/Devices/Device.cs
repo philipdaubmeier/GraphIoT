c719408b42ec5cf008d88af3ab5658e075dee3fc
philipdaubmeier <philip@daubmeier.de>
2020-03-23 21:20:22 +0100

    Added additional device properties that are only included in the gateway feature called device list.

--- src/ViessmannClient/Model/Devices/Device.cs
+++ src/ViessmannClient/Model/Devices/Device.cs
@@ -7,6 +7,10 @@ namespace PhilipDaubmeier.ViessmannClient.Model.Devices
         public long LongId => long.TryParse(Id, out long id) ? id : 0;
         public string? GatewaySerial { get; set; }
         public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Status { get; set; }
+        public string? Fingerprint { get; set; }
         public string? BoilerSerial { get; set; }
         public string? BoilerSerialEditor { get; set; }
         public string? BmuSerial { get; set; }
@@ -14,7 +18,7 @@ namespace PhilipDaubmeier.ViessmannClient.Model.Devices
         public DateTime? CreatedAt { get; set; }
         public DateTime? EditedAt { get; set; }
         public string? ModelId { get; set; }
-        public string? Status { get; set; }
+        public string? ModelVersion { get; set; }
         public string? DeviceType { get; set; }
     }
 }
