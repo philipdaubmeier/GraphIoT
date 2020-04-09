d62040fb71e3ac7b3deb1a27c490b091dfe21c8f
philipdaubmeier <philip@daubmeier.de>
2020-04-09 23:08:43 +0200

    Added model classes for the grafana data query and annotation query requests.

---
+++ src/GraphIoT.Grafana/Model/GrafanaQuery.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace PhilipDaubmeier.GraphIoT.Grafana.Model
+{
+    public class GrafanaQuery
+    {
+        public string Timezone { get; set; } = string.Empty;
+        public int? PanelId { get; set; }
+        public int? DashboardId { get; set; }
+        public Range Range { get; set; } = new Range();
+        public RangeRaw RangeRaw { get; set; } = new RangeRaw();
+        public string Interval { get; set; }
+        public long IntervalMs { get; set; }
+        public List<TargetDefinition> Targets { get; set; } = new List<TargetDefinition>();
+        public List<AdhocFilter>? AdhocFilters { get; set; } = new List<AdhocFilter>();
+        public string Format { get; set; } = string.Empty;
+        public int MaxDataPoints { get; set; }
+    }
+
+    public class TargetDefinition
+    {
+        public CustomQuery? Data { get; set; }
+        public string Target { get; set; } = string.Empty;
+        public string RefId { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+    }
+
+    public class CustomQuery
+    {
+        public string RawMetricId { get; set; } = string.Empty;
+        public List<string>? FilterIfNoneOf { get; set; }
+        public Aggregate? Aggregate { get; set; }
+        public Correction? Correction { get; set; }
+        public int? OverrideMaxDataPoints { get; set; } = null;
+    }
+
+    public class Aggregate
+    {
+        public string Interval { get; set; } = string.Empty;
+        public string Func { get; set; } = string.Empty;
+    }
+
+    public class Correction
+    {
+        public double Factor { get; set; }
+        public double Offset { get; set; }
+    }
+
+    public class AdhocFilter
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Operator { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ src/GraphIoT.Grafana/Model/Range.cs
@@ -0,0 +1,15 @@
+﻿namespace PhilipDaubmeier.GraphIoT.Grafana.Model
+{
+    public class Range
+    {
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+        public RangeRaw? Raw { get; set; }
+    }
+
+    public class RangeRaw
+    {
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+    }
+}
