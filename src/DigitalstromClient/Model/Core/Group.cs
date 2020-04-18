ee361bebafbcd06400984798c560ddb6c09c75bc
philipdaubmeier <philip@daubmeier.de>
2020-04-19 00:11:39 +0200

    Removed dependency to Microsoft.Extensions.Localization

--- src/DigitalstromClient/Model/Core/Group.cs
+++ src/DigitalstromClient/Model/Core/Group.cs
@@ -1,15 +1,8 @@
-﻿using Microsoft.Extensions.Localization;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Options;
-using PhilipDaubmeier.DigitalstromClient.Resources;
+﻿using PhilipDaubmeier.DigitalstromClient.Resources;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
 
 namespace PhilipDaubmeier.DigitalstromClient.Model.Core
 {
