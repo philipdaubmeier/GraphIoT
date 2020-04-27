6e9810d0b1ee9510a41fbdfb6f9f500f07d8f5c7
philipdaubmeier <philip@daubmeier.de>
2020-04-27 20:52:40 +0200

    Added classes for serializing and deserializing cookies and session state to be able to persist and resume a logged in session.

---
+++ src/WeConnectClient/Model/Auth/PersistedCookie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.Auth
+{
+    /// <summary>
+    /// Helper class for serializing and deserializing a Cookie instance with a minimal footprint,
+    /// i.e only attributes that are necessary for the WeConnect portal are persisted:
+    /// name, value, host, path and expiry.
+    /// </summary>
+    internal class PersistedCookie
+    {
+        public PersistedCookie(Cookie cookie) => (Name, Value, Path, Domain, Expires)
+            = (cookie.Name, cookie.Value, cookie.Path, cookie.Domain, cookie.Expires);
+
+        [JsonPropertyName("n")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("v")]
+        public string Value { get; set; }
+
+        [JsonPropertyName("p")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("d")]
+        public string Domain { get; set; }
+
+        [JsonPropertyName("e")]
+        public DateTime Expires { get; set; }
+
+        public Cookie ToCookie(Uri baseUri)
+            => new Cookie(Name, Value, baseUri.AbsolutePath, baseUri.Host)
+            { HttpOnly = true, Secure = true, Expires = Expires };
+    }
+}
---
+++ src/WeConnectClient/Model/Auth/PersistedSession.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.Auth
+{
+    /// <summary>
+    /// Helper class for serializing and deserializing all necessary session data to stay
+    /// logged in even if the application is restarted, i.e. the CSRF token, the portal
+    /// base uri and all cookies including the session cookie containing the cryptographic
+    /// proof of the current login state.
+    /// </summary>
+    internal class PersistedSession
+    {
+        public PersistedSession(string baseJsonUri, string csrf, CookieContainer cookieContainer)
+        {
+            BaseJsonUri = baseJsonUri;
+            Csrf = csrf;
+            Cookies = cookieContainer.GetCookies(BaseUri).Cast<Cookie>().Select(c => new PersistedCookie(c)).ToList();
+        }
+
+        [JsonPropertyName("u")]
+        public string BaseJsonUri { get; set; }
+
+        [JsonPropertyName("csrf")]
+        public string Csrf { get; set; }
+
+        [JsonPropertyName("c")]
+        public List<PersistedCookie> Cookies { get; set; }
+
+        public Uri BaseUri => new Uri(new Uri(BaseJsonUri), "/");
+
+        public void AddToCookieContainer(CookieContainer cookieContainer)
+        {
+            foreach (var cookie in Cookies.Select(c => c.ToCookie(BaseUri)))
+                cookieContainer.Add(cookie);
+        }
+    }
+}
