e11019099d306b6f193c85e7c2cde55742d70be0
philipdaubmeier <philip@daubmeier.de>
2020-04-30 10:48:50 +0200

    Added carfinder model classes

---
+++ src/WeConnectClient/Model/Carfinder/Location.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.Carfinder
+{
+    public class Location
+    {
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("lng")]
+        public double Longitude { get; set; }
+    }
+}
---
+++ src/WeConnectClient/Model/Carfinder/LocationResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.Carfinder
+{
+    internal class LocationResponse : Wiremessage<Location>
+    {
+        [JsonPropertyName("position")]
+        public override Location Body { get; set; } = new Location();
+    }
+}
