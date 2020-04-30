172bc6707c7989784553ec2e5c277ace77b668c7
philipdaubmeier <philip@daubmeier.de>
2020-04-30 14:30:41 +0200

    Added model classes for vehicle list.

---
+++ src/WeConnectClient/Model/VehicleList/VehicleEntry.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.VehicleList
+{
+    public class VehicleEntry
+    {
+        public string Vin { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public bool Expired { get; set; }
+        public string Model { get; set; } = string.Empty;
+        public string ModelCode { get; set; } = string.Empty;
+        public string ModelYear { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string VehicleSpecificFallbackImageUrl { get; set; } = string.Empty;
+        public string ModelSpecificFallbackImageUrl { get; set; } = string.Empty;
+        public string DefaultImageUrl { get; set; } = string.Empty;
+        public string VehicleBrand { get; set; } = string.Empty;
+        public string EnrollmentDate { get; set; } = string.Empty;
+        public bool? DeviceOCU1 { get; set; }
+        public bool? DeviceOCU2 { get; set; }
+        public bool? DeviceMIB { get; set; }
+        public bool EngineTypeCombustian { get; set; }
+        public bool EngineTypeHybridOCU1 { get; set; }
+        public bool EngineTypeHybridOCU2 { get; set; }
+        public bool EngineTypeElectric { get; set; }
+        public bool EngineTypeCNG { get; set; }
+        public bool EngineTypeDefault { get; set; }
+        public string StpStatus { get; set; } = string.Empty;
+        public bool WindowstateSupported { get; set; }
+        public string DashboardUrl { get; set; } = string.Empty;
+        public bool VhrRequested { get; set; }
+        public bool VsrRequested { get; set; }
+        public bool VhrConfigAvailable { get; set; }
+        public bool VerifiedByDealer { get; set; }
+        public bool Vhr2 { get; set; }
+        public bool RoleEnabled { get; set; }
+        public bool IsEL2Vehicle { get; set; }
+        public bool WorkshopMode { get; set; }
+        public bool HiddenUserProfiles { get; set; }
+        public bool? MobileKeyActivated { get; set; }
+        public string EnrollmentType { get; set; } = string.Empty;
+        public bool Ocu3Low { get; set; }
+        public List<PackageService> PackageServices { get; set; }
+        public bool DefaultCar { get; set; }
+        public bool VwConnectPowerLayerAvailable { get; set; }
+        public string XprofileId { get; set; } = string.Empty;
+        public bool? SmartCardKeyActivated { get; set; }
+        public bool FullyEnrolled { get; set; }
+        public bool SecondaryUser { get; set; }
+        public bool Fleet { get; set; }
+        public bool Touareg { get; set; }
+        public bool IceSupported { get; set; }
+        public bool FlightMode { get; set; }
+        public bool EsimCompatible { get; set; }
+        public bool Dkyenabled { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ src/WeConnectClient/Model/VehicleList/VehicleListResponse.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.VehicleList
+{
+    internal class VehicleListResponse : IWiremessage<IEnumerable<VehicleEntry>>
+    {
+        public string ErrorCode { get; set; } = string.Empty;
+
+        public bool HasError => !int.TryParse(ErrorCode, out int errorCode) || errorCode != 0;
+
+        [JsonIgnore]
+        public IEnumerable<VehicleEntry> Body
+        {
+            get
+            {
+                foreach (var entry in FullyLoadedVehiclesResponse.CompleteVehicles)
+                    yield return entry;
+
+                foreach (var entry in FullyLoadedVehiclesResponse.VehiclesNotFullyLoaded)
+                    yield return entry;
+            }
+        }
+
+        internal VehicleListContainer FullyLoadedVehiclesResponse { get; set; } = new VehicleListContainer();
+    }
+
+    internal class VehicleListContainer
+    {
+        public List<VehicleEntry> CompleteVehicles { get; set; } = new List<VehicleEntry>();
+
+        public List<VehicleEntry> VehiclesNotFullyLoaded { get; set; } = new List<VehicleEntry>();
+
+        public string Status { get; set; } = string.Empty;
+
+        public bool CurrentVehicleValid { get; set; }
+    }
+}
