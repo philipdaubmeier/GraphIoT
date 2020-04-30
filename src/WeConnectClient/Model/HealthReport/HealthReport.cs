3cf5049979f7fa062fd2fcaf0329c126a81303a7
philipdaubmeier <philip@daubmeier.de>
2020-04-30 21:05:47 +0200

    Added model classes for health report.

---
+++ src/WeConnectClient/Model/HealthReport/HealthReport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.HealthReport
+{
+    public class HealthReport
+    {
+        public string CreationDate { get; set; } = string.Empty;
+        public string CreationTime { get; set; } = string.Empty;
+        public List<string> DiagnosticMessages { get; set; } = new List<string>();
+        public bool HasValidData { get; set; }
+        public string ReportId { get; set; } = string.Empty;
+        public bool IsOlderSixMonths { get; set; }
+        public string MileageValue { get; set; } = string.Empty;
+        public long Timestamp { get; set; }
+        public HeaderData HeaderData { get; set; } = new HeaderData();
+        public string JobStatus { get; set; } = string.Empty;
+        public bool FetchFailed { get; set; }
+    }
+
+    public class HeaderData
+    {
+        public string RangeMileage { get; set; } = string.Empty;
+        public List<string> LastRefreshTime { get; set; } = new List<string>();
+        public bool ServiceOverdue { get; set; }
+        public bool OilOverdue { get; set; }
+        public string Mileage { get; set; } = string.Empty;
+        public bool ShowOil { get; set; }
+        public string VhrNoDataText { get; set; } = string.Empty;
+        public bool ShowService { get; set; }
+    }
+}
---
+++ src/WeConnectClient/Model/HealthReport/HealthReportResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.HealthReport
+{
+    internal class HealthReportResponse : Wiremessage<List<HealthReport>>
+    {
+        [JsonPropertyName("vehicleHealthReportList")]
+        public override List<HealthReport> Body { get; set; } = new List<HealthReport>();
+
+        [JsonPropertyName("X-RateLimit-CreationTask-Reset")]
+        public string RateLimitReset { get; set; } = string.Empty;
+
+        [JsonPropertyName("X-RateLimit-CreationTask-Limit")]
+        public string RateLimitMax { get; set; } = string.Empty;
+
+        [JsonPropertyName("X-RateLimit-CreationTask-Remaining")]
+        public string RateLimitRemaining { get; set; } = string.Empty;
+    }
+}
