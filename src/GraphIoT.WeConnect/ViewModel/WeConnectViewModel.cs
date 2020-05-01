9e18e4cbb64dd906a404a07f304175f0e478bf49
philipdaubmeier <philip@daubmeier.de>
2020-05-01 23:37:42 +0200

    Implemented view model classes and according localization strings in GraphIoT.WeConnect

---
+++ src/GraphIoT.WeConnect/ViewModel/WeConnectViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Localization;
+using PhilipDaubmeier.CompactTimeSeries;
+using PhilipDaubmeier.GraphIoT.Core.ViewModel;
+using PhilipDaubmeier.GraphIoT.WeConnect.Database;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.WeConnect.ViewModel
+{
+    public class WeConnectViewModel : GraphCollectionViewModelMultiResolutionBase<WeConnectData>
+    {
+        public WeConnectViewModel(IWeConnectDbContext databaseContext, IStringLocalizer<WeConnectViewModel> localizer)
+            : base(new Dictionary<Resolution, IQueryable<WeConnectData>>() {
+                       { Resolution.LowRes, databaseContext.WeConnectLowresDataSet },
+                       { Resolution.MidRes, databaseContext.WeConnectDataSet }
+                   },
+                   Enumerable.Range(0, 8).ToDictionary(x => x.ToString(), x => x),
+                   localizer)
+        { }
+
+        public override string Key => "weconnect";
+
+        public override GraphViewModel Graph(int index)
+        {
+            return index switch
+            {
+                0 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Driven kilometers"], null, _localizer["# km"]),
+                1 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Battery SoC"], null, _localizer["# %"]),
+                2 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Consumed kWh"], null, _localizer["#.# kWh"]),
+                3 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Average consumption kWh/100 km"], null, _localizer["#.# kWh/100 km"]),
+                4 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Average speed km/h"], null, _localizer["#.# km/h"]),
+                5 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Charging state"], null, _localizer["# (charging/not charging)"]),
+                6 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Climate temperature"], null, _localizer["#.# °C"]),
+                7 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Climate state"], null, _localizer["# (on/off)"]),
+                8 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Window melt state"], null, _localizer["# (on/off)"]),
+                9 => DeferredLoadGraph<TimeSeries<int>, int>(index, _localizer["Remote heating state"], null, _localizer["# (on/off)"]),
+                _ => new GraphViewModel()
+            };
+        }
+    }
+}
