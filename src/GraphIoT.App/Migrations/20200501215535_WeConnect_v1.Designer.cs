e6ab28b2709715db2e38f8d7e4e5408e82532773
philipdaubmeier <philip@daubmeier.de>
2020-05-02 00:04:36 +0200

    Added migration for new database tables.

--- src/GraphIoT.App/Migrations/20200501215535_WeConnect_v1.Designer.cs
+++ src/GraphIoT.App/Migrations/20200501215535_WeConnect_v1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhilipDaubmeier.GraphIoT.App.Database;
 
 namespace PhilipDaubmeier.GraphIoT.App.Migrations
 {
     [DbContext(typeof(PersistenceContext))]
-    partial class PersistenceContextModelSnapshot : ModelSnapshot
+    [Migration("20200501215535_WeConnect_v1")]
+    partial class WeConnect_v1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GraphIoT.App/Migrations/20200501215535_WeConnect_v1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhilipDaubmeier.GraphIoT.App.Migrations
+{
+    public partial class WeConnect_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DsEnergyLowresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyLowresDataSet");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DsEnergyMidresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyMidresDataSet");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CircuitId",
+                table: "DsEnergyMidresDataSet",
+                maxLength: 34,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 34,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CircuitId",
+                table: "DsEnergyLowresDataSet",
+                maxLength: 34,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 34,
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "WeConnectDataSet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Vin = table.Column<string>(maxLength: 20, nullable: false),
+                    DrivenKilometersCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    BatterySocCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    ConsumedKwhCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    AverageConsumptionCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    AverageSpeedCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    ChargingStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    ClimateTempCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    ClimateStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    WindowMeltStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    RemoteHeatingStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    Day = table.Column<DateTime>(nullable: false),
+                    Mileage = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeConnectDataSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeConnectLowresDataSet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Vin = table.Column<string>(maxLength: 20, nullable: false),
+                    DrivenKilometersCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    BatterySocCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    ConsumedKwhCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    AverageConsumptionCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    AverageSpeedCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    ChargingStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    ClimateTempCurve = table.Column<string>(maxLength: 800, nullable: true),
+                    ClimateStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    WindowMeltStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    RemoteHeatingStateCurve = table.Column<string>(maxLength: 100, nullable: true),
+                    Month = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeConnectLowresDataSet", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DsEnergyLowresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyLowresDataSet",
+                column: "CircuitId",
+                principalTable: "DsCircuits",
+                principalColumn: "Dsuid",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DsEnergyMidresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyMidresDataSet",
+                column: "CircuitId",
+                principalTable: "DsCircuits",
+                principalColumn: "Dsuid",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DsEnergyLowresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyLowresDataSet");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DsEnergyMidresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyMidresDataSet");
+
+            migrationBuilder.DropTable(
+                name: "WeConnectDataSet");
+
+            migrationBuilder.DropTable(
+                name: "WeConnectLowresDataSet");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CircuitId",
+                table: "DsEnergyMidresDataSet",
+                maxLength: 34,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 34);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CircuitId",
+                table: "DsEnergyLowresDataSet",
+                maxLength: 34,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 34);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DsEnergyLowresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyLowresDataSet",
+                column: "CircuitId",
+                principalTable: "DsCircuits",
+                principalColumn: "Dsuid",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DsEnergyMidresDataSet_DsCircuits_CircuitId",
+                table: "DsEnergyMidresDataSet",
+                column: "CircuitId",
+                principalTable: "DsCircuits",
+                principalColumn: "Dsuid",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
