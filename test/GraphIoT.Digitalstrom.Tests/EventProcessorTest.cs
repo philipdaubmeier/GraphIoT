945dcc7a1db7b196c6deb452bccad412eaa922a9
philipdaubmeier <philip@daubmeier.de>
2020-05-05 14:48:52 +0200

    Made integration test more robust against timing issues / race conditions.

--- test/GraphIoT.Digitalstrom.Tests/EventProcessorTest.cs
+++ test/GraphIoT.Digitalstrom.Tests/EventProcessorTest.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PhilipDaubmeier.DigitalstromClient.Model.Core;
 using PhilipDaubmeier.DigitalstromClient.Model.Events;
 using PhilipDaubmeier.DigitalstromDssMock;
+using PhilipDaubmeier.DigitalstromTimeSeriesApi.Database;
 using RichardSzalay.MockHttp;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,17 +34,24 @@ namespace PhilipDaubmeier.GraphIoT.Digitalstrom.Tests
             // We should see that the database is empty at first, with no recorded digitalstrom events
             Assert.Null(db.DsSceneEventDataSet.FirstOrDefault()?.EventStreamEncoded);
 
+            static async Task DelayUntilDataWritten(IntegrationTestDbContext db, int expected)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    await Task.Delay(100);
+                    if (db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStreamEncoded != null &&
+                        db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream.Count() > 1 &&
+                        db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault().EventStream.Last().Properties.SceneID == expected)
+                        break;
+                }
+            }
+
             // After 100ms (see configured ItemCollectionInterval, plus buffer) the event processor hosted service
             // should have written all ds events to the database that we have mocked with the test setup
-            for (int i = 0; i < 100; i++)
-            {
-                await Task.Delay(100);
-                if (db.DsSceneEventDataSet.FirstOrDefault()?.EventStreamEncoded != null)
-                    break;
-            }
-            Assert.NotNull(db.DsSceneEventDataSet.FirstOrDefault()?.EventStreamEncoded);
+            await DelayUntilDataWritten(db, (int)SceneCommand.Preset0);
+            Assert.NotNull(db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStreamEncoded);
 
-            var storedEvents = db.DsSceneEventDataSet.FirstOrDefault()?.EventStream;
+            var storedEvents = db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream;
             Assert.True(storedEvents.Count() > 1);
             Assert.Equal(32027, (int)storedEvents.Last().Properties.ZoneID);
             Assert.Equal((int)SceneCommand.Preset0, (int)storedEvents.First().Properties.SceneID);
@@ -53,18 +62,18 @@ namespace PhilipDaubmeier.GraphIoT.Digitalstrom.Tests
             mockHttp.Flush();
 
             // Change the returned event content and reset the database
-            _factory.MockedEventResponse.Respond("application/json", SceneCommand.Alarm1.ToMockedSceneEvent());
             await _factory.InitDb();
-
             await Task.Delay(200);
+
+            _factory.MockedEventResponse.Respond("application/json", SceneCommand.Alarm1.ToMockedSceneEvent());
 
             mockHttp.Flush();
 
-            await Task.Delay(200);
+            await DelayUntilDataWritten(db, (int)SceneCommand.Alarm1);
 
-            storedEvents = db.DsSceneEventDataSet.FirstOrDefault()?.EventStream;
+            storedEvents = db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream;
             Assert.NotNull(storedEvents);
-            Assert.Equal((int)SceneCommand.Preset0, (int)storedEvents.Last().Properties.SceneID);
+            Assert.Equal((int)SceneCommand.Alarm1, (int)storedEvents.Last().Properties.SceneID);
         }
     }
 }
