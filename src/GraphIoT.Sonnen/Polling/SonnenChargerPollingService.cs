4ce8cf633c9bba6139229e7f749ece345a37ab47
philipdaubmeier <philip@daubmeier.de>
2020-05-05 22:47:32 +0200

    Extended TimeSeriesDbEntityBase to simplify all polling services.

--- src/GraphIoT.Sonnen/Polling/SonnenChargerPollingService.cs
+++ src/GraphIoT.Sonnen/Polling/SonnenChargerPollingService.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using PhilipDaubmeier.CompactTimeSeries;
 using PhilipDaubmeier.GraphIoT.Sonnen.Database;
 using PhilipDaubmeier.SonnenClient;
 using System;
@@ -74,32 +73,18 @@ namespace PhilipDaubmeier.GraphIoT.Sonnen.Polling
             dbChargerSeries.SetSeries(0, series1);
             dbChargerSeries.ChargedEnergyTotal = chargedEnergyTotal;
 
-            var series2 = dbChargerSeries.ActivePowerSeries;
-            series2[time] = activePower;
-            dbChargerSeries.SetSeries(1, series2);
+            dbChargerSeries.SetSeriesValue(1, time, activePower);
+            dbChargerSeries.SetSeriesValue(2, time, current);
+            dbChargerSeries.SetSeriesValue(3, time, connected);
+            dbChargerSeries.SetSeriesValue(4, time, charging);
+            dbChargerSeries.SetSeriesValue(5, time, smartMode);
 
-            var series3 = dbChargerSeries.CurrentSeries;
-            series3[time] = current;
-            dbChargerSeries.SetSeries(2, series3);
-
-            var series4 = dbChargerSeries.ConnectedSeries;
-            series4[time] = connected;
-            dbChargerSeries.SetSeries(3, series4);
-
-            var series5 = dbChargerSeries.ChargingSeries;
-            series5[time] = charging;
-            dbChargerSeries.SetSeries(4, series5);
-
-            var series6 = dbChargerSeries.SmartModeSeries;
-            series6[time] = smartMode;
-            dbChargerSeries.SetSeries(5, series6);
-
-            SaveLowresChargerValues(dbContext, day, series1, series2, series3, series4, series5, series6);
+            SaveLowresChargerValues(dbContext, day, dbChargerSeries);
 
             dbContext.SaveChanges();
         }
 
-        private static void SaveLowresChargerValues(ISonnenDbContext dbContext, DateTime day, TimeSeries<double> series1Src, TimeSeries<double> series2Src, TimeSeries<double> series3Src, TimeSeries<bool> series4Src, TimeSeries<bool> series5Src, TimeSeries<bool> series6Src)
+        private static void SaveLowresChargerValues(ISonnenDbContext dbContext, DateTime day, SonnenChargerMidresData midRes)
         {
             static DateTime FirstOfMonth(DateTime date) => date.AddDays(-1 * (date.Day - 1));
             var month = FirstOfMonth(day);
@@ -107,35 +92,12 @@ namespace PhilipDaubmeier.GraphIoT.Sonnen.Polling
             if (dbChargerSeries == null)
                 dbContext.SonnenChargerLowresDataSet.Add(dbChargerSeries = new SonnenChargerLowresData() { Key = month });
 
-            var series1 = dbChargerSeries.ChargedEnergySeries;
-            var resampler1 = new TimeSeriesResampler<TimeSeries<double>, double>(series1.Span) { Resampled = series1 };
-            resampler1.SampleAggregate(series1Src, x => x.Average());
-            dbChargerSeries.SetSeries(0, resampler1.Resampled);
-
-            var series2 = dbChargerSeries.ActivePowerSeries;
-            var resampler2 = new TimeSeriesResampler<TimeSeries<double>, double>(series2.Span) { Resampled = series2 };
-            resampler2.SampleAggregate(series2Src, x => x.Average());
-            dbChargerSeries.SetSeries(1, resampler2.Resampled);
-
-            var series3 = dbChargerSeries.CurrentSeries;
-            var resampler3 = new TimeSeriesResampler<TimeSeries<double>, double>(series3.Span) { Resampled = series3 };
-            resampler3.SampleAggregate(series3Src, x => x.Average());
-            dbChargerSeries.SetSeries(2, resampler3.Resampled);
-
-            var series4 = dbChargerSeries.ConnectedSeries;
-            var resampler4 = new TimeSeriesResampler<TimeSeries<bool>, bool>(series4.Span) { Resampled = series4 };
-            resampler4.SampleAggregate(series4Src, x => x.Any(v => v));
-            dbChargerSeries.SetSeries(3, resampler4.Resampled);
-
-            var series5 = dbChargerSeries.ChargingSeries;
-            var resampler5 = new TimeSeriesResampler<TimeSeries<bool>, bool>(series5.Span) { Resampled = series5 };
-            resampler5.SampleAggregate(series5Src, x => x.Any(v => v));
-            dbChargerSeries.SetSeries(4, resampler5.Resampled);
-
-            var series6 = dbChargerSeries.SmartModeSeries;
-            var resampler6 = new TimeSeriesResampler<TimeSeries<bool>, bool>(series6.Span) { Resampled = series6 };
-            resampler6.SampleAggregate(series6Src, x => x.Any(v => v));
-            dbChargerSeries.SetSeries(5, resampler6.Resampled);
+            dbChargerSeries.ResampleFrom<double>(midRes, 0, x => x.Average());
+            dbChargerSeries.ResampleFrom<double>(midRes, 1, x => x.Average());
+            dbChargerSeries.ResampleFrom<double>(midRes, 2, x => x.Average());
+            dbChargerSeries.ResampleFrom<bool>(midRes, 3, x => x.Any(v => v));
+            dbChargerSeries.ResampleFrom<bool>(midRes, 4, x => x.Any(v => v));
+            dbChargerSeries.ResampleFrom<bool>(midRes, 5, x => x.Any(v => v));
         }
     }
 }
