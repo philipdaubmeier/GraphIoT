c7489a981003f6b6693a62b86fcb5a70eca1e274
philipdaubmeier <philip@daubmeier.de>
2020-05-05 22:53:20 +0200

    Added TimeSeries attribute.

---
+++ src/GraphIoT.Core/Database/TimeSeriesAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhilipDaubmeier.GraphIoT.Core.Database
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class TimeSeriesAttribute : Attribute
+    {
+        public Type Type { get; }
+
+        public TimeSeriesAttribute(Type type) => Type = type;
+    }
+}
