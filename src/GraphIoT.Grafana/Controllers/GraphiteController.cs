e5973e4bb0a4c25d8c23ba92edfbdf1a106f6b00
philipdaubmeier <philip@daubmeier.de>
2020-05-15 15:18:45 +0200

    Added first minimal viable increment of render method that actually returns the graph data in graphite api.

--- src/GraphIoT.Grafana/Controllers/GraphiteController.cs
+++ src/GraphIoT.Grafana/Controllers/GraphiteController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using PhilipDaubmeier.GraphIoT.Core.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
 {
@@ -38,9 +42,37 @@ namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
 
         // POST: api/graphite/render
         [HttpPost("render")]
-        public ActionResult Render()
+        public async Task<ActionResult> Render()
         {
-            return StatusCode((int)HttpStatusCode.OK);
+            var formData = await Request.ReadFormAsync();
+            if (!formData.TryGetValue("target", out StringValues targetRaw)
+                || !formData.TryGetValue("from", out StringValues fromRaw)
+                || !formData.TryGetValue("until", out StringValues untilRaw)
+                || !formData.TryGetValue("format", out StringValues formatRaw)
+                || !formData.TryGetValue("maxDataPoints", out StringValues maxPointsRaw))
+                return StatusCode((int)HttpStatusCode.OK);
+
+            var from = DateTimeOffset.FromUnixTimeSeconds(int.Parse(fromRaw.FirstOrDefault())).UtcDateTime;
+            var until = DateTimeOffset.FromUnixTimeSeconds(int.Parse(untilRaw.FirstOrDefault())).UtcDateTime;
+            var maxDataPoints = int.Parse(maxPointsRaw.FirstOrDefault());
+            if (!(formatRaw.FirstOrDefault()?.Equals("json", StringComparison.InvariantCultureIgnoreCase) ?? false))
+                return StatusCode((int)HttpStatusCode.BadRequest);
+
+            List<dynamic[]> GetDataPoints(string target)
+            {
+                var splitted = target.Split('.');
+                if (splitted.Length < 2 || !int.TryParse(splitted[1], out int index) || index < 0
+                    || !graphViewModels.ContainsKey(splitted[0]) || index >= graphViewModels[splitted[0]].GraphCount())
+                    return new List<dynamic[]>();
+                var viewModel = graphViewModels[splitted[0]];
+                return viewModel.Graph(index).TimestampedPoints().ToList();
+            }
+
+            return Json(targetRaw.Select(t => new
+            {
+                target = t,
+                datapoints = GetDataPoints(t)
+            }));
         }
 
         // GET: api/graphite/metrics/find
@@ -49,7 +81,7 @@ namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
         [SuppressMessage("Style", "IDE0060", Justification = "'from' and 'until' are passed in by grafana but not used to exclude graphs.")]
         public ActionResult FindMetrics(int from, int until, string query)
         {
-            query = string.IsNullOrWhiteSpace(query) || !query.EndsWith('*') ? string.Empty : query.Substring(0, query.Length - 1);
+            query = string.IsNullOrWhiteSpace(query) || !query.EndsWith('*') ? string.Empty : query[0..^1];
 
             return Json(GraphIds.Where(g => g.StartsWith(query, StringComparison.InvariantCultureIgnoreCase))
                 .Select(g => g.Substring(query.Length))
