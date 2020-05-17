82566af1c2bd2080adfbc3710735293f7a0d62c3
philipdaubmeier <philip@daubmeier.de>
2020-05-17 21:55:02 +0200

    Added graphite expression classes which are used to build the AST to be built by the graphite parser.

---
+++ src/GraphIoT.Graphite/Model/Query/IGraphiteExpression.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    public interface IGraphiteExpression
+    {
+        IEnumerable<IGraphiteGraph> Graphs { get; }
+    }
+}
---
+++ src/GraphIoT.Graphite/Model/Query/SourceExpression.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    public class SourceExpression : IGraphiteExpression
+    {
+        public IEnumerable<IGraphiteGraph> Graphs { get; }
+
+        public SourceExpression(List<IGraphiteGraph> graphs) => Graphs = graphs;
+    }
+}
