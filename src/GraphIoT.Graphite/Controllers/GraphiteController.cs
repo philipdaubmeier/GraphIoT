bb038f86360413ad52bb2e9dd6350bfd56855976
philipdaubmeier <philip@daubmeier.de>
2020-05-19 00:23:00 +0200

    Using the new data source class and parser in GraphiteController to parse and evaluate query expression and return the transformed graphs.

--- src/GraphIoT.Graphite/Controllers/GraphiteController.cs
+++ src/GraphIoT.Graphite/Controllers/GraphiteController.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
+using PhilipDaubmeier.CompactTimeSeries;
 using PhilipDaubmeier.GraphIoT.Core.ViewModel;
+using PhilipDaubmeier.GraphIoT.Graphite.Model;
 using PhilipDaubmeier.GraphIoT.Graphite.Parser;
+using PhilipDaubmeier.GraphIoT.Graphite.Parser.Query;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
@@ -19,25 +21,11 @@ namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
     [Route("api/graphite")]
     public class GraphiteController : Controller
     {
-        private List<string>? graphIds = null;
-        public List<string> GraphIds
-        {
-            get
-            {
-                if (graphIds is null)
-                    graphIds = graphViewModels.SelectMany(n => n.Value.Graphs().Select(g => $"{n.Key}.{g.Key}".Replace('_', '.'))).ToList();
-
-                return graphIds;
-            }
-        }
-
-        private readonly Dictionary<string, IGraphCollectionViewModel> graphViewModels;
-        private readonly Dictionary<string, IEventCollectionViewModel> eventViewModels;
+        private readonly GraphDataSource dataSource;
 
         public GraphiteController(IEnumerable<IGraphCollectionViewModel> graphViewModels, IEnumerable<IEventCollectionViewModel> eventViewModels)
         {
-            this.graphViewModels = graphViewModels.ToDictionary(x => x.Key, x => x);
-            this.eventViewModels = eventViewModels.ToDictionary(x => x.Key, x => x);
+            dataSource = new GraphDataSource(graphViewModels);
         }
 
         // POST: api/graphite/render
@@ -45,7 +33,7 @@ namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
         public async Task<ActionResult> Render()
         {
             var formData = await Request.ReadFormAsync();
-            if (!formData.TryGetValue("target", out StringValues targetRaw)
+            if (!formData.TryGetValue("target", out StringValues targets)
                 || !formData.TryGetValue("from", out StringValues fromRaw)
                 || !formData.TryGetValue("until", out StringValues untilRaw)
                 || !formData.TryGetValue("format", out StringValues formatRaw)
@@ -58,27 +46,14 @@ namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
             if (!(formatRaw.FirstOrDefault()?.Equals("json", StringComparison.InvariantCultureIgnoreCase) ?? false))
                 return StatusCode((int)HttpStatusCode.BadRequest);
 
-            List<dynamic[]> GetDataPoints(string target)
-            {
-                var splitted = target.Split('.');
-                if (splitted.Length < 2 || !int.TryParse(splitted[1], out int index) || index < 0
-                    || !graphViewModels.ContainsKey(splitted[0]) || index >= graphViewModels[splitted[0]].GraphCount())
-                    return new List<dynamic[]>();
-                var viewModel = graphViewModels[splitted[0]];
-                return viewModel.Graph(index).TimestampedPoints().ToList();
-            }
+            dataSource.Span = new TimeSeriesSpan(from.ToUniversalTime(), until.ToUniversalTime(), maxDataPoints);
 
-            IEnumerable<string> GetTargets(string query)
+            var parser = new Parser() { DataSource = dataSource };
+            return Json(targets.SelectMany(target => parser.Parse(target).Graphs).Select(g => new
             {
-                var q = new TargetQuery(query);
-                return GraphIds.Where(g => q.IsMatch(g));
-            }
-
-            return Json(targetRaw.SelectMany(tq => GetTargets(tq).Select(t => new
-            {
-                target = t,
-                datapoints = GetDataPoints(t)
-            })));
+                target = g.Name,
+                datapoints = g.TimestampedPoints()
+            }));
         }
 
         // GET: api/graphite/metrics/find
@@ -89,7 +64,7 @@ namespace PhilipDaubmeier.GraphIoT.Grafana.Controllers
         {
             var q = new TargetQuery(query);
 
-            return Json(GraphIds.Where(g => q.IsMatch(g)).Select(g => q.NextSegment(g))
+            return Json(dataSource.GraphKeys.Where(g => q.IsMatch(g)).Select(g => q.NextSegment(g))
                 .Select(g => new { g.expandable, text = g.segment }).Distinct());
         }
 
