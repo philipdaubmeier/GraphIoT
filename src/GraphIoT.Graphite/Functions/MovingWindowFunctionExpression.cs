b7b2f8f7727c3443d330963fa9c16fdde86c8b36
philipdaubmeier <philip@daubmeier.de>
2020-05-18 22:25:53 +0200

    Added MovingWindowFunction that internally uses ResampledGraph which in turn uses the timeseries resampler to aggregate values.

---
+++ src/GraphIoT.Graphite/Functions/MovingWindowFunctionExpression.cs
@@ -0,0 +1,20 @@
+﻿using PhilipDaubmeier.GraphIoT.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    public class MovingWindowFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly TimeSpan _spacing;
+        private readonly Aggregator _func;
+
+        public IEnumerable<IGraphiteGraph> Graphs => _innerExpression.Graphs
+            .Select(g => new ResampledGraph(g, _spacing, _func));
+
+        public MovingWindowFunctionExpression(IGraphiteExpression innerExpression, TimeSpan spacing, Aggregator func)
+            => (_innerExpression, _spacing, _func) = (innerExpression, spacing, func);
+    }
+}
---
+++ src/GraphIoT.Graphite/Model/Graph/ResampledGraph.cs
@@ -0,0 +1,51 @@
+﻿using PhilipDaubmeier.CompactTimeSeries;
+using PhilipDaubmeier.GraphIoT.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    public class ResampledGraph : GraphiteGraph
+    {
+        private readonly IGraphiteGraph _operand;
+        private readonly Aggregator _func;
+
+        public override string Name => _operand.Name;
+        public override DateTime Begin => _operand.Begin;
+        public override TimeSpan Spacing { get; }
+
+        public override IEnumerable<double?> Points
+        {
+            get
+            {
+                var series = ToTimeSeries(Points, Begin, Spacing);
+                var newSpan = new TimeSeriesSpan(series.Begin, series.End, Spacing);
+                var resampler = new TimeSeriesResampler<TimeSeries<double>, double>(newSpan, SamplingConstraint.NoOversampling);
+                Func<IEnumerable<double>, double> aggregate = _func switch
+                {
+                    Aggregator.Minimum => x => x.Min(),
+                    Aggregator.Maximum => x => x.Max(),
+                    Aggregator.Sum => x => x.Sum(),
+                    _ => x => x.Average()
+                };
+
+                resampler.SampleAggregate(series, aggregate);
+                return resampler.Resampled.Select(x => x.Value);
+            }
+        }
+
+        public ResampledGraph(IGraphiteGraph operand, TimeSpan spacing, Aggregator func)
+            => (_operand, Spacing, _func) = (operand, spacing, func);
+
+        private static TimeSeries<double> ToTimeSeries(IEnumerable<double?> points, DateTime begin, TimeSpan spacing)
+        {
+            var values = points.ToList();
+            var series = new TimeSeries<double>(new TimeSeriesSpan(begin, begin + spacing * values.Count, values.Count));
+            var i = 0;
+            foreach (var value in values)
+                series[i++] = value;
+            return series;
+        }
+    }
+}
