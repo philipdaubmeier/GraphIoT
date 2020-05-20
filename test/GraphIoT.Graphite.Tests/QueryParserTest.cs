832e6d7c19377526ae692cb1ad9219fd26727adc
philipdaubmeier <philip@daubmeier.de>
2020-05-20 23:33:05 +0200

    Started implementing unit test for parser.

---
+++ test/GraphIoT.Graphite.Tests/QueryParserTest.cs
@@ -0,0 +1,54 @@
+﻿using PhilipDaubmeier.GraphIoT.Core.ViewModel;
+using PhilipDaubmeier.GraphIoT.Graphite.Model;
+using PhilipDaubmeier.GraphIoT.Graphite.Parser.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GraphIoT.Graphite.Tests
+{
+    public class QueryParserTest
+    {
+        private class MockGraphViewModel : GraphViewModel
+        {
+            public MockGraphViewModel() : base()
+            {
+                Begin = new DateTime(2020, 1, 1, 12, 0, 0, DateTimeKind.Utc);
+                Spacing = new TimeSpan(0, 1, 0);
+                Name = "unit.test";
+                Key = Name;
+                Points = new List<dynamic>() { 0.01d, 0.08d, 0.2d, 0.7d, 0.9d, 1.0d, 1.1d, 0.8d, 0.3d, -0.2d };
+            }
+        }
+
+        private class MockGraphCollectionViewModel : GraphCollectionViewModelBase
+        {
+            public override string Key => "mock";
+            protected override void InvalidateData() { }
+            public override int GraphCount() => 1;
+            public override IEnumerable<string> GraphKeys() => Graphs().Select(g => g.Key);
+            public override GraphViewModel Graph(int index) => new MockGraphViewModel();
+            public override IEnumerable<GraphViewModel> Graphs() => new[] { Graph(0) };
+        }
+
+        [Theory]
+        [InlineData("movingWindow(mock.unit.test,'3min','avg')")]
+        [InlineData("movingWindow(mock.unit.test, '3min', 'avg')")]
+        [InlineData("      movingWindow(mock.unit.test,'3min','avg')    ")]
+        [InlineData("  \t \r\n  movingWindow \n\n  ( \n  mock.unit.test    , \r\n '3min'   ,  \r\n  'avg'  )  \t \r\n  ")]
+        public void ParserWhitepaceInvarianceTest(string query)
+        {
+            var dataSource = new GraphDataSource(new[] { new MockGraphCollectionViewModel() });
+            var parser = new Parser() { DataSource = dataSource };
+            var ast = parser.Parse(query);
+
+            Assert.IsType<MovingWindowFunctionExpression>(ast);
+            Assert.IsType<ResampledGraph>(ast.Graphs.First());
+            Assert.Equal("unit.test", ((ResampledGraph)ast.Graphs.First()).Name);
+            Assert.Equal(0.097d, ((ResampledGraph)ast.Graphs.First()).Points.First()!.Value, 3);
+            Assert.Equal(0.800d, ((ResampledGraph)ast.Graphs.First()).Points.Skip(1).First()!.Value, 3);
+            Assert.Equal(0.967d, ((ResampledGraph)ast.Graphs.First()).Points.Skip(2).First()!.Value, 3);
+        }
+    }
+}
