5899a1c2b8dd6eefae4671c4a7553724a9ceba54
philipdaubmeier <philip@daubmeier.de>
2020-05-31 23:46:48 +0200

    Added attribute classes to annotate function expression classes with their metainfo, like the graphite function name, group and suggestions for parameters.

---
+++ src/GraphIoT.Graphite/Model/Attributes/GraphiteFunctionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class GraphiteFunctionAttribute : Attribute
+    {
+        public string Name { get; }
+        public string Group { get; }
+
+        public GraphiteFunctionAttribute(string name, string group) => (Name, Group) = (name, group);
+    }
+}
---
+++ src/GraphIoT.Graphite/Model/Attributes/GraphiteParamAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class GraphiteParamAttribute : Attribute
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public bool Required { get; }
+        public IList<string> Suggestions { get; }
+
+        public GraphiteParamAttribute(string name, string type, bool required, string? suggestions = null)
+            => (Name, Type, Required, Suggestions) = (name, type, required, ToSuggestionList(suggestions));
+
+        private static IList<string> ToSuggestionList(string? suggestions)
+            => suggestions?.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Distinct().ToList() ?? new List<string>();
+    }
+}
