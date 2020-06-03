3bab7d8f8792eeb4f73444f87fde87965013e806
philipdaubmeier <philip@daubmeier.de>
2020-06-03 15:44:39 +0200

    Added asPercent function

---
+++ src/GraphIoT.Graphite/Functions/AsPercentExpression.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [GraphiteFunction("asPercent", "Calculate")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    [GraphiteParam("total", "seriesList", true)]
+    public class AsPercentExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly IGraphiteGraph _total;
+
+        public IEnumerable<IGraphiteGraph> Graphs => _innerExpression.Graphs
+            .Select(g => new DerivedGraphBinary(g, _total, (x, t) => x.HasValue && t.HasValue ? x.Value / t.Value * 100 : (double?)null));
+
+        public AsPercentExpression(IGraphiteExpression innerExpression, IGraphiteExpression total)
+            => (_innerExpression, _total) = (innerExpression, total.Graphs.FirstOrDefault() ?? new NullGraph());
+    }
+}
---
+++ src/GraphIoT.Graphite/Model/Graph/NullGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    public class NullGraph : GraphiteGraph
+    {
+        public override string Name => string.Empty;
+        public override DateTime Begin => DateTime.MinValue;
+        public override TimeSpan Spacing => TimeSpan.FromSeconds(1);
+
+        public override IEnumerable<double?> Points
+        {
+            get { yield break; }
+        }
+
+        public NullGraph() { }
+    }
+}
