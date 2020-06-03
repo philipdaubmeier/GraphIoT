45b6aed28d37ec862b96bf906aad229e24672fb9
philipdaubmeier <philip@daubmeier.de>
2020-06-03 15:44:23 +0200

    Added more advanced alias function with regex replacement option.

--- src/GraphIoT.Graphite/Functions/AliasFunctionExpression.cs
+++ src/GraphIoT.Graphite/Functions/AliasFunctionExpression.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PhilipDaubmeier.GraphIoT.Graphite.Model
 {
@@ -16,5 +17,22 @@ namespace PhilipDaubmeier.GraphIoT.Graphite.Model
 
         public AliasFunctionExpression(IGraphiteExpression innerExpression, string alias)
             => (_innerExpression, _alias) = (innerExpression, alias);
+    }
+
+    [GraphiteFunction("aliasSub", "Alias")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    [GraphiteParam("search", "string", true)]
+    [GraphiteParam("replace", "string", true)]
+    public class AliasSubFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly Regex _search;
+        private readonly string _replace;
+
+        public IEnumerable<IGraphiteGraph> Graphs => _innerExpression.Graphs
+            .Select(g => new DerivedGraphUnary(g, x => x, n => _search.Replace(n, _replace)));
+
+        public AliasSubFunctionExpression(IGraphiteExpression innerExpression, string search, string replace)
+            => (_innerExpression, _search, _replace) = (innerExpression, new Regex(search), replace);
     }
 }
