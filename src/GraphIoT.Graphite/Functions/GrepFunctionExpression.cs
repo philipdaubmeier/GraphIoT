8e1223a43218113a13df7a38f1bb0e97660242ba
philipdaubmeier <philip@daubmeier.de>
2020-06-05 23:50:02 +0200

    Added grep and exclude functions.

---
+++ src/GraphIoT.Graphite/Functions/GrepFunctionExpression.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [GraphiteFunction("grep", "Filter Series")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    [GraphiteParam("grep", "string", true)]
+    public class GrepFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly Regex _grep;
+
+        public IEnumerable<IGraphiteGraph> Graphs =>
+            _innerExpression.Graphs.Where(g => _grep.IsMatch(g.Name));
+
+        public GrepFunctionExpression(IGraphiteExpression innerExpression, string grep)
+            => (_innerExpression, _grep) = (innerExpression, new Regex(grep));
+    }
+
+    [GraphiteFunction("exclude", "Filter Series")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    [GraphiteParam("exclude", "string", true)]
+    public class ExcludeFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly Regex _exclude;
+
+        public IEnumerable<IGraphiteGraph> Graphs =>
+            _innerExpression.Graphs.Where(g => !_exclude.IsMatch(g.Name));
+
+        public ExcludeFunctionExpression(IGraphiteExpression innerExpression, string exclude)
+            => (_innerExpression, _exclude) = (innerExpression, new Regex(exclude));
+    }
+}
