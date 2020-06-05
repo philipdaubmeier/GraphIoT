d3a90daf16236ae67b5b9a0014a99c870349e7e9
philipdaubmeier <philip@daubmeier.de>
2020-06-05 23:51:55 +0200

    Added combine function count() that returns the number of selected series.

---
+++ src/GraphIoT.Graphite/Functions/CountSeriesFunctionExpression.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [GraphiteFunction("countSeries", "Combine")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    public class CountSeriesFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly int _graphCount;
+
+        public IEnumerable<IGraphiteGraph> Graphs => _innerExpression.Graphs
+            .Select(g => new DerivedGraphUnary(g, x => _graphCount, n => n));
+
+        public CountSeriesFunctionExpression(IGraphiteExpression innerExpression)
+            => (_innerExpression, _graphCount) = (innerExpression, innerExpression.Graphs.Count());
+    }
+}
