454a9ec4611e1e69fe9dbdaafd955e4a8b8eb8b6
philipdaubmeier <philip@daubmeier.de>
2020-06-05 23:52:26 +0200

    Added filter functions currentBelow and currentAbove.

---
+++ src/GraphIoT.Graphite/Functions/CurrentAboveFunctionExpression.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [GraphiteFunction("currentAbove", "Filter Series")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    [GraphiteParam("n", "int", true)]
+    public class CurrentAboveFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly double _n;
+
+        public IEnumerable<IGraphiteGraph> Graphs =>
+            _innerExpression.Graphs.Where(g => (g.Points.LastOrDefault() ?? double.MinValue) >= _n);
+
+        public CurrentAboveFunctionExpression(IGraphiteExpression innerExpression, double n)
+            => (_innerExpression, _n) = (innerExpression, n);
+    }
+
+    [GraphiteFunction("currentBelow", "Filter Series")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    [GraphiteParam("n", "int", true)]
+    public class CurrentBelowFunctionExpression : IGraphiteExpression
+    {
+        private readonly IGraphiteExpression _innerExpression;
+        private readonly double _n;
+
+        public IEnumerable<IGraphiteGraph> Graphs =>
+            _innerExpression.Graphs.Where(g => (g.Points.LastOrDefault() ?? double.MinValue) <= _n);
+
+        public CurrentBelowFunctionExpression(IGraphiteExpression innerExpression, double n)
+            => (_innerExpression, _n) = (innerExpression, n);
+    }
+}
