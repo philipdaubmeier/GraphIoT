d155a8c84141bb07928ecc8c45797e9729b01329
philipdaubmeier <philip@daubmeier.de>
2020-06-29 16:52:28 +0200

    Added new graphite function "isNull" which outputs 1 for null values and 0 for any other value. This can be used to detect or count any null values in the existing data set.

---
+++ src/GraphIoT.Graphite/Functions/IsNullFunctionExpression.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.GraphIoT.Graphite.Model
+{
+    [GraphiteFunction("isNull", "Transform")]
+    [GraphiteParam("seriesList", "seriesList", true)]
+    public class IsNullFunctionExpression : IFunctionExpression
+    {
+        public IGraphiteExpression InnerExpression { get; }
+
+        public IEnumerable<IGraphiteGraph> Graphs => InnerExpression.Graphs
+            .Select(g => new DerivedGraphUnary(g, x => x is null ? 1 : 0, n => n));
+
+        public IsNullFunctionExpression(IGraphiteExpression innerExpression)
+            => InnerExpression = innerExpression;
+    }
+}
