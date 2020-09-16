77d6b97a13983fbb6df8a542f5df4d63f0ae5ad5
philipdaubmeier <philip@daubmeier.de>
2020-09-16 17:33:57 +0200

    Issue #15: Added additional fields to Message type as it is used in the logbook feature with different properties.

--- src/ViessmannClient/Model/Features/Message.cs
+++ src/ViessmannClient/Model/Features/Message.cs
@@ -3,9 +3,13 @@
     public class Message
     {
         public string? Timestamp { get; set; }
+        public string? Actor { get; set; }
         public string? ErrorCode { get; set; }
-        public string? Status { get; set; }
+        public object? Status { get; set; }
+        public string? @Event { get; set; }
+        public string? StateMachine { get; set; }
         public int? Count { get; set; }
         public string? Priority { get; set; }
+        public int? AdditionalInfo { get; set; }
     }
 }
