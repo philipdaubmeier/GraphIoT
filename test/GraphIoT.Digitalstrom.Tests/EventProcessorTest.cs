952ee553d55ccd240755dca6ac182ee87f979ed7
philipdaubmeier <philip@daubmeier.de>
2021-06-28 16:24:31 +0200

    Made event processor unit test more stable (it sometimes had concurrency issues and false negatives)

--- test/GraphIoT.Digitalstrom.Tests/EventProcessorTest.cs
+++ test/GraphIoT.Digitalstrom.Tests/EventProcessorTest.cs
@@ -5,6 +5,7 @@ using PhilipDaubmeier.DigitalstromClient.Model.Events;
 using PhilipDaubmeier.DigitalstromDssMock;
 using PhilipDaubmeier.DigitalstromTimeSeriesApi.Database;
 using RichardSzalay.MockHttp;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -30,50 +31,64 @@ namespace PhilipDaubmeier.GraphIoT.Digitalstrom.Tests
             // Init the mock for http requests to the digitalstrom server and mock for the database
             var mockHttp = _factory.Server.Host.Services.GetRequiredService<MockHttpMessageHandler>();
             var db = await _factory.InitDb();
+            Assert.NotNull(mockHttp);
 
             // We should see that the database is empty at first, with no recorded digitalstrom events
             Assert.Null(db.DsSceneEventDataSet.FirstOrDefault()?.EventStreamEncoded);
 
-            static async Task DelayUntilDataWritten(IntegrationTestDbContext db, int expected)
+            async Task<IEnumerable<DssEvent>> DelayUntilDataWritten(IntegrationTestDbContext db, int expected)
             {
-                for (int i = 0; i < 100; i++)
+                for (int i = 0; i < 200; i++)
                 {
                     await Task.Delay(100);
-                    if (db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStreamEncoded != null &&
-                        db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream.Count() > 1 &&
-                        db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream.Last().Properties.SceneID == expected)
-                        break;
+
+                    if (mockHttp is null)
+                        continue;
+                    mockHttp.Flush();
+
+                    if (db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStreamEncoded is null)
+                        continue;
+
+                    var expectedItems = db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream
+                                          .Where(x => x.Properties.SceneID == expected).ToList();
+                    if (expectedItems is not null && expectedItems.Count > 0)
+                        return expectedItems;
                 }
+                return new List<DssEvent>();
             }
 
             // After 100ms (see configured ItemCollectionInterval, plus buffer) the event processor hosted service
             // should have written all ds events to the database that we have mocked with the test setup
-            await DelayUntilDataWritten(db, (int)SceneCommand.Preset0);
+            var storedEvents1 = await DelayUntilDataWritten(db, (int)SceneCommand.Preset0);
             Assert.NotNull(db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStreamEncoded);
 
-            var storedEvents = db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream;
-            Assert.True(!(storedEvents is null) && storedEvents.Count() > 1);
-            Assert.Equal(32027, storedEvents is null ? -1 : (int)storedEvents.Last().Properties.ZoneID);
-            Assert.Equal((int)SceneCommand.Preset0, storedEvents is null ? -1 : (int)storedEvents.First().Properties.SceneID);
-            Assert.Equal((int)SystemEvent.CallScene, storedEvents is null ? -1 : (int)storedEvents.First().SystemEvent.Type);
+            Assert.NotNull(storedEvents1);
+            if (storedEvents1 is null)
+                return;
+            Assert.NotEmpty(storedEvents1);
+            Assert.Equal(32027, (int)storedEvents1.Last().Properties.ZoneID);
+            Assert.Equal((int)SceneCommand.Preset0, (int)storedEvents1.First().Properties.SceneID);
+            Assert.Equal((int)SystemEvent.CallScene, (int)storedEvents1.First().SystemEvent.Type);
 
             // Turn off event sending
             mockHttp.AutoFlush = false;
             mockHttp.Flush();
 
             // Change the returned event content and reset the database
+            db.DsSceneEventDataSet.RemoveRange(db.DsSceneEventDataSet);
+            await db.SaveChangesAsync();
             await _factory.InitDb();
-            await Task.Delay(200);
+            await Task.Delay(100);
 
             _factory.MockedEventResponse.Respond("application/json", SceneCommand.Alarm1.ToMockedSceneEvent());
 
-            mockHttp.Flush();
+            var storedEvents2 = await DelayUntilDataWritten(db, (int)SceneCommand.Alarm1);
 
-            await DelayUntilDataWritten(db, (int)SceneCommand.Alarm1);
-
-            storedEvents = db.DsSceneEventDataSet.AsNoTracking().FirstOrDefault()?.EventStream;
-            Assert.NotNull(storedEvents);
-            Assert.Equal((int)SceneCommand.Alarm1, storedEvents is null ? -1 : (int)storedEvents.Last().Properties.SceneID);
+            Assert.NotNull(storedEvents2);
+            if (storedEvents2 is null)
+                return;
+            Assert.NotEmpty(storedEvents2);
+            Assert.NotEmpty(storedEvents2.Where(x => x.Properties.SceneID == (int)SceneCommand.Alarm1));
         }
     }
 }
