9bfb321663e5e85c24a3eec3510e43eaf7cfcd00
philipdaubmeier <philip@daubmeier.de>
2021-10-18 11:28:28 +0200

    Added model classes for fuel status api repsonse

---
+++ src/WeConnectClient/Model/Fuel/FuelStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.Fuel
+{
+    public class FuelStatus
+    {
+        public string Id { get; set; } = string.Empty;
+        public DateTime CarCapturedTimestamp { get; set; } = DateTime.MinValue;
+
+        public string EngineType => Properties.FirstOrDefault(x => x.Name == "engineType")?.Value ?? string.Empty;
+        public double? RemainingRangeKm => double.TryParse(Properties.FirstOrDefault(x => x.Name == "remainingRange_km")?.Value, out double val) ? val : null;
+        public double? CurrentFuelLevelPercent => double.TryParse(Properties.FirstOrDefault(x => x.Name == "currentFuelLevel_pct")?.Value, out double val) ? val : null;
+        public double? CurrentSocPercent => double.TryParse(Properties.FirstOrDefault(x => x.Name == "currentSOC_pct")?.Value, out double val) ? val : null;
+
+        public List<FuelStatusProperty> Properties { get; set; } = new();
+    }
+
+    public class FuelStatusProperty
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ src/WeConnectClient/Model/Fuel/FuelStatusResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PhilipDaubmeier.WeConnectClient.Model.Fuel
+{
+    internal class FuelStatusResponse : IWiremessage<List<FuelStatus>>
+    {
+        public string ErrorCode => string.Empty;
+
+        public bool HasError => false;
+
+        [JsonPropertyName("data")]
+        public virtual List<FuelStatus> Body { get; set; } = new();
+    }
+}
